76ef4baf310f2de4893590eca5ba785468d438e0
Hideo Yoneyama <hideoyon@outlook.com>
2025-01-31 16:39:08 +0900

    PythonProcessor

---
+++ MultiversalRenerder/PythonProssesor/PythonProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Python.Runtime;
+using MultiversalRenderer.Interfaces;
+namespace PythonProcessor
+{
+
+    internal class PythonProcessor : IAsyncDisposable, IMultiversalScriptProcessor
+    {
+        public PythonProcessor() { }
+
+        public IMultiversalScope multiversalscope { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public object callfunction(object functionobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object callfunction(object functionobject, object scope, object thisObj, object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object compile(string script)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object execute(string script)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void put(string name, object val)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MultiversalRenerder/PythonProssesor/PythonProcessorScope.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Python.Runtime;
+using MultiversalRenderer.Interfaces;
+using System.Threading.Tasks;
+public class PythonProcessorScope : IAsyncDisposable, IMultiversalScope
+{
+    public ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ___disposeScriptEngine()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ___getMultivasalScopeName()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string[] ___getMultiversalInvokeScriptNames()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IMultiversalScriptProcessor ___getMultiversalScriptProcessor()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IMultiversalWindow ___getMultiversalWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IMultiversalWindowType ___getMutilversalWindowType()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ___initScriptEngine()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ___isDefaultMultiversalProcessor()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ___isInitCompleted()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ___relaseMultiversal()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ___setMultiversalWindow(IMultiversalWindow window)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ___setMutilversalWindowType(IMultiversalWindowType windowType)
+    {
+        throw new NotImplementedException();
+    }
+}
